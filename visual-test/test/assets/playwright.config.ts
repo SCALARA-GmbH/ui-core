1cd0a0e5f586482bfd32c8a81e2345d8acb0e9d2
armin jazi <aj@scalara.de>
2021-08-18 17:53:11 +0200

    Add asset tests

---
+++ visual-test/test/assets/playwright.config.ts
@@ -0,0 +1,14 @@
+import { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  projects: [
+    {
+      name: 'Chrome Stable',
+      use: {
+        browserName: 'chromium',
+        viewport: { width: 1080, height: 1080 }
+      }
+    }
+  ]
+};
+export default config;
